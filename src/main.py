279e2a5d1dab520258e2dd949f4c810c5c57e0da
Mateo Torres Arredondo <mateotorres2409@gmail.com>
2024-09-17 19:38:40 -0500

    Se mapean los otros valores para download y upload

--- src/main.py
+++ src/main.py
@@ -7,25 +7,33 @@ import os
 ping_latency_metric = Gauge('ping_latency', 'ping_latency_metric')
 download_bandwidth_metric = Gauge('download_bandwidth', 'download_bandwidth_metric')
 upload_bandwidth_metric = Gauge('upload_bandwidth', 'upload_bandwidth_metric')
+download_bytes_metric = Gauge('download_bytes', 'download_bytes_metric')
+upload_bytes_metric = Gauge('upload_bytes', 'upload_bytes_metric')
+download_elapsed_metric = Gauge('download_elapsed', 'download_elapsed_metric')
+upload_elapsed_metric = Gauge('upload_elapsed', 'upload_elapsed_metric')
 
-def speedtest(units):
-    response = subprocess.Popen('./speedtest --accept-gdpr -u '+units+' -f json-pretty', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+def speedtest():
+    response = subprocess.Popen('./speedtest -f json-pretty', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
     return json.loads(response)
 
-def getMetrics(units):
-    response = speedtest(units)
+def getMetrics():
+    response = speedtest()
     ping_latency_metric.set(response['ping']['latency'])
     download_bandwidth_metric.set(response['download']['bandwidth'])
     upload_bandwidth_metric.set(response['upload']['bandwidth'])
+    download_bytes_metric.set(response['download']['bytes'])
+    upload_bytes_metric.set(response['upload']['bytes'])
+    download_elapsed_metric.set(response['download']['elapsed'])
+    upload_elapsed_metric.set(response['upload']['elapsed'])
+    print(response['type']+" - "+response['2024-09-18T00:33:49Z'])
 
 if __name__ == "__main__":
-    units =  os.environ.get("UNITS_SPEEDTEST", "bps")
-    sleep = os.environ.get("SLEEP_SPEEDTEST", 120)
-    port = os.environ.get("PORT_METRICS", 8000)
+    sleep = os.environ.get("SLEEP_SPEEDTEST", 900)
+    port = os.environ.get("PORT_METRICS", 9090)
     start_http_server(int(port))
-    getMetrics(units)
+    getMetrics()
     while True:
         time.sleep(int(sleep))
-        getMetrics(units)
+        getMetrics()
 else:
     print("File one executed when imported")
