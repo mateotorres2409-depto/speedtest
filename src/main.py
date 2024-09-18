ce043279e74622fbe068f4a4e92f8d3c34a3d3f5
Mateo Torres Arredondo <mateotorres2409@gmail.com>
2024-09-17 20:04:00 -0500

    Ajuste para controlar erro de cli

--- src/main.py
+++ src/main.py
@@ -14,26 +14,34 @@ upload_elapsed_metric = Gauge('upload_elapsed', 'upload_elapsed_metric')
 
 def speedtest():
     response = subprocess.Popen('./speedtest -f json-pretty', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
-    return json.loads(response)
+    try:
+        rsJson = json.loads(response) 
+    except:
+        rsJson = {}
+    
+    return rsJson
 
-def getMetrics():
+def getMetrics():    
     response = speedtest()
-    ping_latency_metric.set(response['ping']['latency'])
-    download_bandwidth_metric.set(response['download']['bandwidth'])
-    upload_bandwidth_metric.set(response['upload']['bandwidth'])
-    download_bytes_metric.set(response['download']['bytes'])
-    upload_bytes_metric.set(response['upload']['bytes'])
-    download_elapsed_metric.set(response['download']['elapsed'])
-    upload_elapsed_metric.set(response['upload']['elapsed'])
-    print(response['type']+" - "+response['2024-09-18T00:33:49Z'])
+    if response != {}:        
+        ping_latency_metric.set(response['ping']['latency'])
+        download_bandwidth_metric.set(response['download']['bandwidth'])
+        upload_bandwidth_metric.set(response['upload']['bandwidth'])
+        download_bytes_metric.set(response['download']['bytes'])
+        upload_bytes_metric.set(response['upload']['bytes'])
+        download_elapsed_metric.set(response['download']['elapsed'])
+        upload_elapsed_metric.set(response['upload']['elapsed'])
+        print(response['type']+" - "+response['2024-09-18T00:33:49Z'])
+    else:
+        print("Se genero error!")
 
 if __name__ == "__main__":
     sleep = os.environ.get("SLEEP_SPEEDTEST", 900)
     port = os.environ.get("PORT_METRICS", 9090)
     start_http_server(int(port))
     getMetrics()
-    #while True:
-    #    time.sleep(int(sleep))
-    #    getMetrics()
+    while True:
+        time.sleep(int(sleep))
+        getMetrics()
 else:
     print("File one executed when imported")
